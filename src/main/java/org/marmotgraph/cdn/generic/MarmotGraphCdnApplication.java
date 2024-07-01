b39b22d715a3ef64c402acfb31dfb41568ec9843
Oliver Schmid <oliver.schmid@cscs.ch>
2024-07-01 14:28:42 +0200

    initial commit

---
+++ src/main/java/org/marmotgraph/cdn/generic/MarmotGraphCdnApplication.java
@@ -0,0 +1,13 @@
+package org.marmotgraph.cdn.generic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarmotGraphCdnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarmotGraphCdnApplication.class, args);
+	}
+
+}
