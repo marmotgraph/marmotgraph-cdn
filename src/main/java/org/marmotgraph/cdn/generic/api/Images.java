8802ec0e3c02edd1579e4aef7d188a475b4a5126
annapaola <annapaola.santarsiero@gmail.com>
2024-07-03 22:01:10 +0200

    Alternative way to get images and CSS without an explicit method for each profile.
    So now you can ask for
    
    /v1/images/generic/favicon
    /v1/images/ebrains/favicon
    
    /v1/css/generic
    /v1/css/ebrains
    
    and content will be provided using a single method.

--- src/main/java/org/marmotgraph/cdn/generic/api/Images.java
+++ src/main/java/org/marmotgraph/cdn/generic/api/Images.java
@@ -3,20 +3,16 @@ package org.marmotgraph.cdn.generic.api;
 import io.swagger.v3.oas.annotations.Operation;
 import org.marmotgraph.cdn.generic.utils.Constants;
 import org.marmotgraph.cdn.generic.utils.Utils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/" + Constants.API_VERSION + "/images")
 public class Images {
 
-    @GetMapping(value = "/favicon", produces = Constants.IMAGE_ICO)
+    @GetMapping(value = "/{type}/favicon", produces = Constants.IMAGE_ICO)
     @Operation(description = "This is the favicon to be used by all UIs")
-    public @ResponseBody byte[] favicon() {
-        return Utils.getStaticResource("generic/favicon.ico");
+    public @ResponseBody byte[] favIconResource(@PathVariable("type") String type) {
+        return Utils.getStaticResource(type + "/favicon.ico");
     }
-
 
 }
